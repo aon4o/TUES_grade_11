6d7e24595ebb26a6a076439b293f164dd63fa9a7
aon2003 <a.o.naidenov@gmail.com>
2021-05-31 09:21:52 +0300

    In Subject OS / C: 	Updated 'ls' - 600 / 750 points

--- Operating Systems/14. ls/functions.c
+++ Operating Systems/14. ls/functions.c
@@ -7,8 +7,12 @@
 // File with some functions for helping the work of the program.
 //---------------------------------------------
 #include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
-char get_entry_type(struct stat* entry_stat) {
+char get_entry_type(struct stat* entry_stat)
+{
 	unsigned int mode = entry_stat->st_mode;
 	if (S_ISREG(mode)){
 		return '-';
@@ -24,5 +28,104 @@ char get_entry_type(struct stat* entry_stat) {
 		return 'l';
 	} else if (S_ISSOCK(mode)){
 		return 's';
+	} else {
+		return ' ';
 	}
+}
+
+void print_total_blocks(char dir_path[NAME_MAX], int flag_a)
+{
+//	OPENING THE DIRECTORY
+	DIR *dir = opendir(dir_path);
+	if (dir == NULL)
+	{
+		perror("opendir");
+		return;
+	}
+
+//	GETTING THE NUMBER OF BLOCKS
+	struct dirent *dir_entry;
+	long total = 0;
+	struct stat entry_stat;
+
+	while ((dir_entry = readdir(dir)) != NULL)
+	{
+		if(flag_a) {
+			if(!strcmp(dir_entry->d_name, ".") || !strcmp(dir_entry->d_name, "..")) {
+				continue;
+			}
+		} else {
+			if(dir_entry->d_name[0] == '.') {
+				continue;
+			}
+		}
+
+		char entry_path[NAME_MAX] = "\0";
+		strcat(entry_path, dir_path);
+		strcat(entry_path, "/");
+		strcat(entry_path, dir_entry->d_name);
+
+		int stat_value = stat(entry_path, &entry_stat);
+		if (stat_value != 0)
+		{
+			fprintf(stderr, "stat: %s", dir_path);
+			return;
+		}
+
+		total += entry_stat.st_blocks;
+	}
+
+	printf("total %ld\n", total/2);
+
+//	CLOSING THE DIRECTORY
+	if (closedir(dir) != 0)
+	{
+		perror("closedir");
+		return;
+	}
+}
+
+void print_long_listing_format(struct stat* entry_stat)
+{
+	unsigned int mode = entry_stat->st_mode;
+
+//	PRINTING PERMISSIONS
+	printf("%c", get_entry_type(entry_stat));
+	printf((mode & S_IRUSR) ? "r" : "-");
+	printf((mode & S_IWUSR) ? "w" : "-");
+	printf((mode & S_IXUSR) ? "x" : "-");
+	printf((mode & S_IRGRP) ? "r" : "-");
+	printf((mode & S_IWGRP) ? "w" : "-");
+	printf((mode & S_IXGRP) ? "x" : "-");
+	printf((mode & S_IROTH) ? "r" : "-");
+	printf((mode & S_IWOTH) ? "w" : "-");
+	printf((mode & S_IXOTH) ? "x" : "-");
+	printf(" %li ", entry_stat->st_nlink);
+
+//	PRINTING USER AND GROUP
+	struct passwd* user;
+	struct group* group;
+
+	if((user = getpwuid(entry_stat->st_uid)) == NULL) {
+		perror("getpwuid");
+	}
+	if((group = getgrgid(entry_stat->st_gid)) == NULL) {
+		perror("getgrgid");
+	}
+
+	printf("%s ", user->pw_name);
+	printf("%s ", group->gr_name);
+
+//	PRINTING SIZE AND TIMESTAMP
+	struct tm *time_now;
+	char time_string[100];
+	time_t file_time = entry_stat->st_mtime;
+
+	if((time_now = localtime(&file_time)) == NULL) {
+		perror("localtime");
+	}
+	strftime(time_string, sizeof(time_string), "%b %d %R", time_now);
+
+	printf("%ld ", entry_stat->st_size);
+	printf("%s ", time_string);
 }
--- Operating Systems/14. ls/main.c
+++ Operating Systems/14. ls/main.c
@@ -12,68 +12,14 @@
 
 #include "functions.c"
 
+
 int main(int argc, char **argv)
 {
-//	CHECKING FOR CASE WITH NO ARGUMENTS
-	if (argc < 2)
-	{
-//		GETTING THE PATH TO THE CURRENT DIRECTORY
-		char dir_path[PATH_MAX];
-		if (getcwd(dir_path, sizeof(dir_path)) == NULL)
-		{
-			perror("getcwd");
-			return 1;
-		}
-
-//		OPENING THE DIRECTORY
-		DIR *dir = opendir(dir_path);
-		if (dir == NULL)
-		{
-			perror("opendir");
-			return 1;
-		}
-
-//		DOING THE WORK
-		struct dirent *dir_entry;
-		struct stat entry_stat;
-		while ((dir_entry = readdir(dir)) != NULL)
-		{
-//			CHECKING IF THE FILE IS HIDDEN
-			if(dir_entry->d_name[0] == '.') {
-				continue;
-			}
-
-			char entry_path[NAME_MAX] = "\0";
-			strcat(entry_path, dir_path);
-			strcat(entry_path, "/");
-			strcat(entry_path, dir_entry->d_name);
-
-			int stat_value = stat(entry_path, &entry_stat);
-			if (stat_value != 0)
-			{
-				fprintf(stderr, "stat: %s", dir_path);
-				return 1;
-			}
-			printf("%c ", get_entry_type(&entry_stat));
-			printf("%s\n", dir_entry->d_name);
-		}
-
-//		CLOSING THE DIRECTORY
-		if (closedir(dir) != 0)
-		{
-			perror("closedir");
-			return 1;
-		}
-		return 0;
-	}
-
 //	CHECKING FOR OPTIONAL ARGUMENTS
 	int flag_a = 0, flag_l = 0, flag_r = 0;
 	int option;
-	// put ':' at the starting of the string so compiler can distinguish between '?' and ':'
-	while((option = getopt(argc, argv, "lAR")) != -1){ //get option from the getopt() method
+	while((option = getopt(argc, argv, "lAR")) != -1) {
 		switch(option){
-			//For option i, r, l, print that these are options
 			case 'A':
 				flag_a = 1;
 				break;
@@ -91,20 +37,27 @@ int main(int argc, char **argv)
 		}
 	}
 
-	for(; optind < argc; optind++)
-	{
-//		TEST
-//		printf("%s\n", argv[optind]);
-
-//		GETTING THE NAME OF THE FILE OR THE PATH TO THE DIRECTORY DIRECTORY
-//		char argument[PATH_MAX];
+//	CHECKING FOR NON OPTIONAL ARGUMENTS
+	if (optind == argc) {
+//		GETTING THE PATH TO THE CURRENT DIRECTORY
+		char dir_path[PATH_MAX];
+		if (getcwd(dir_path, sizeof(dir_path)) == NULL)
+		{
+			perror("getcwd");
+			return 1;
+		}
 
 //		OPENING THE DIRECTORY
-		DIR *dir = opendir(argv[optind]);
+		DIR *dir = opendir(dir_path);
 		if (dir == NULL)
 		{
 			perror("opendir");
 			return 1;
+		}
+
+//		PRINTING THE TOTAL
+		if (flag_l) {
+			print_total_blocks(dir_path, flag_a);
 		}
 
 //		DOING THE WORK
@@ -113,22 +66,34 @@ int main(int argc, char **argv)
 		while ((dir_entry = readdir(dir)) != NULL)
 		{
 //			CHECKING IF THE FILE IS HIDDEN
-			if(dir_entry->d_name[0] == '.') {
-				continue;
+			if(flag_a) {
+				if(!strcmp(dir_entry->d_name, ".") || !strcmp(dir_entry->d_name, "..")) {
+					continue;
+				}
+			} else {
+				if(dir_entry->d_name[0] == '.') {
+					continue;
+				}
 			}
 
 			char entry_path[NAME_MAX] = "\0";
-			strcat(entry_path, argv[optind]);
+			strcat(entry_path, dir_path);
 			strcat(entry_path, "/");
 			strcat(entry_path, dir_entry->d_name);
 
 			int stat_value = stat(entry_path, &entry_stat);
 			if (stat_value != 0)
 			{
-				fprintf(stderr, "stat: %s", argv[optind]);
+				fprintf(stderr, "stat: %s", dir_path);
 				return 1;
 			}
-			printf("%c ", get_entry_type(&entry_stat));
+
+			if(flag_l) {
+				print_long_listing_format(&entry_stat);
+			} else {
+				printf("%c ", get_entry_type(&entry_stat));
+			}
+
 			printf("%s\n", dir_entry->d_name);
 		}
 
@@ -141,5 +106,101 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
+//	IF THERE ARE NON OPTIONAL ARGUMENTS
+	char previous_option = ' ';
+	for(; optind < argc; ++optind)
+	{
+//		GETTING THE TYPE OF THE CURRENT ARGUMENT
+		struct stat argument_stat;
+		int stat_value = stat(argv[optind], &argument_stat);
+		if (stat_value != 0)
+		{
+			fprintf(stderr, "ls: cannot access %s: No such file or directory\n", argv[optind]);
+			continue;
+		}
+
+//		IF NOT DIRECTORY
+		if (get_entry_type(&argument_stat) != 'd')
+		{
+			if (previous_option == 'd') {
+				printf("\n");
+			}
+
+			if(flag_l) {
+				print_long_listing_format(&argument_stat);
+			} else {
+				printf("%c ", get_entry_type(&argument_stat));
+			}
+			printf("%s\n", argv[optind]);
+			previous_option = get_entry_type(&argument_stat);
+		}
+//		IF DIRECTORY
+		else
+		{
+//			PRINTING THE PATH
+			if (argc > 2) {
+				if (previous_option != ' ') {
+					printf("\n");
+				}
+				printf("%s:\n", argv[optind]);
+			}
+
+//			OPENING THE DIRECTORY
+			DIR *dir = opendir(argv[optind]);
+			if (dir == NULL)
+			{
+				fprintf(stderr, "ls: cannot open directory %s/: Permission denied\n", argv[optind]);
+				continue;
+			}
+
+//			PRINTING THE TOTAL
+			if (flag_l) {
+				print_total_blocks(argv[optind], flag_a);
+			}
+
+//			DOING THE WORK
+			struct dirent *dir_entry;
+			struct stat entry_stat;
+			while ((dir_entry = readdir(dir)) != NULL)
+			{
+//				CHECKING IF THE FILE IS HIDDEN
+				if(flag_a) {
+					if(!strcmp(dir_entry->d_name, ".") || !strcmp(dir_entry->d_name, "..")) {
+						continue;
+					}
+				} else {
+					if(dir_entry->d_name[0] == '.') {
+						continue;
+					}
+				}
+
+				char entry_path[NAME_MAX] = "\0";
+				strcat(entry_path, argv[optind]);
+				strcat(entry_path, "/");
+				strcat(entry_path, dir_entry->d_name);
+				if (stat(entry_path, &entry_stat) != 0)
+				{
+					fprintf(stderr, "stat: %s", argv[optind]);
+					return 1;
+				}
+
+				if(flag_l) {
+					print_long_listing_format(&entry_stat);
+				} else {
+					printf("%c ", get_entry_type(&entry_stat));
+				}
+
+				printf("%s\n", dir_entry->d_name);
+			}
+
+//			CLOSING THE DIRECTORY
+			if (closedir(dir) != 0)
+			{
+				perror("closedir");
+				return 1;
+			}
+			previous_option = get_entry_type(&argument_stat);
+		}
+	}
 	return 0;
 }
